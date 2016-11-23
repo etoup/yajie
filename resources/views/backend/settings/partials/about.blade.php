354519fe3fcf5cc75cf1b4310d4b15ab4639fb14
etoup <etoupcom@126.com>
2016-11-23 17:17:02 +0800

    edit

--- resources/views/backend/settings/partials/about.blade.php
+++ resources/views/backend/settings/partials/about.blade.php
@@ -13,7 +13,7 @@
             <p id="social">
                 <a class="github-button" href="https://github.com/austintoddj/canvas" data-count-href="/austintoddj/canvas/stargazers" data-count-api="/repos/austintoddj/canvas#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star austintoddj/canvas on GitHub">Star</a>
                 <a class="github-button" href="https://github.com/austintoddj/canvas/fork" data-count-href="/austintoddj/canvas/network" data-count-api="/repos/austintoddj/canvas#forks_count" data-count-aria-label="# forks on GitHub" aria-label="Fork austintoddj/canvas on GitHub">Fork</a>
-                <a href="https://twitter.com/austintoddj" class="twitter-follow-button" data-show-count="false">Follow @austintoddj</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+                <a href="https://twitter.com/austintoddj" class="twitter-follow-button" data-show-count="false">Follow @austintoddj</a>
             </p>
             <p>Code released under the <a href="https://github.com/austintoddj/Canvas/blob/master/LICENSE" target="_blank">MIT License</a></p>
             <p>Coded by <a href="https://github.com/austintoddj" target="_blank">Todd Austin</a> and an <a href="https://github.com/austintoddj/canvas/graphs/contributors" target="_blank">awesome group of contributors</a></p>
