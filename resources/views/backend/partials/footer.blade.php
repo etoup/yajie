f01237ca2872356ec04442177b19e20ec1e687ad
etoup <etoupcom@126.com>
2016-11-23 17:21:20 +0800

    edit

--- resources/views/backend/partials/footer.blade.php
+++ resources/views/backend/partials/footer.blade.php
@@ -1,13 +1,13 @@
 <footer id="footer">
-    Thank you for creating with <a href="http://canvas.toddaustin.io" target="_blank">Canvas</a>&nbsp;&#183;&nbsp; {{ App\Models\Settings::canvasVersion() }}
+    欢迎使用 <a href="http://canvas.toddaustin.io" target="_blank">Canvas</a>系统&nbsp;&#183;&nbsp; {{ App\Models\Settings::canvasVersion() }}
 
     <ul class="f-menu">
-        <li><a href="{{ url('admin') }}">Home</a></li>
-        <li><a href="{{ url('admin/post') }}">Posts</a></li>
-        <li><a href="{{ url('admin/tag') }}">Tags</a></li>
-        <li><a href="{{ url('admin/upload') }}">Media</a></li>
-        <li><a href="{{ url('admin/tools') }}">Tools</a></li>
-        <li><a href="{{ url('admin/settings') }}">Settings</a></li>
-        <li><a href="mailto:austin.todd.j@gmail.com">Support</a></li>
+        <li><a href="{{ url('admin') }}">控制台</a></li>
+        <li><a href="{{ url('admin/post') }}">文章</a></li>
+        <li><a href="{{ url('admin/tag') }}">标签</a></li>
+        <li><a href="{{ url('admin/upload') }}">媒体</a></li>
+        <li><a href="{{ url('admin/tools') }}">工具</a></li>
+        <li><a href="{{ url('admin/settings') }}">设置</a></li>
+        <li><a href="mailto:austin.todd.j@gmail.com">支持帮助</a></li>
     </ul>
 </footer>
